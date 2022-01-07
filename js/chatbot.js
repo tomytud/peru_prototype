7a7dac4faacbf35e7a0ba2af42e1949eed630395
Tomy Hommel <theownmry@gmail.com>
2022-01-07 10:07:00 +0100

    agent js

---
+++ js/chatbot.js
@@ -0,0 +1,8 @@
+const agent = document.querySelector('df-messenger');
+var check = 0;
+agent.addEventListener('df-response-received', function (event) {
+
+
+    if (event.detail.response.queryResult.action = "input.welcome" && check == 0) {
+        inputHint = agent.shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > input");
+        inputHint.placeholder ="Antwort eingeben";
