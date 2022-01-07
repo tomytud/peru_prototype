bd1a1ebf93aadd825b2ab9326bbe1697d0f405eb
Tomy Hommel <theownmry@gmail.com>
2022-01-07 10:09:22 +0100

    agent js input message

--- js/chatbot.js
+++ js/chatbot.js
@@ -5,4 +5,5 @@ agent.addEventListener('df-response-received', function (event) {
 
     if (event.detail.response.queryResult.action = "input.welcome" && check == 0) {
         inputHint = agent.shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > input");
-        inputHint.placeholder ="Antwort eingeben";
+        inputHint.placeholder ="Antwort eingeben";}
+    });
